7be4fd692716a966e39e4a7cb848ba2785d6618f
Ω <om3ga6400@proton.me>
2026-05-13 18:55:24 +0000

    works

---
+++ aoty.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         AOTY UI fixes
+// @namespace    https://github.com/om3ga6400
+// @version      0.1.0
+// @description  Remove the critic score box on album pages.
+// @author       om3ga6400
+// @match        https://www.albumoftheyear.org/album/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  var node = document.querySelector(".albumCriticScoreBox");
+  if (node) {
+    node.remove();
+  }
+
+  var userScoreHeading = document.querySelector(".albumUserScoreBox .heading");
+  if (userScoreHeading) {
+    userScoreHeading.remove();
+  }
+
+  var noUnderlineLinks = document.querySelectorAll("a.noUnderline");
+  noUnderlineLinks.forEach(function (link) {
+    link.remove();
+  });
+
+  var criticsSection = document.getElementById("critics");
+  if (criticsSection) {
+    criticsSection.remove();
+  }
+
+  var mustHearButton = document.querySelector(".mustHearButton.user");
+  if (mustHearButton) {
+    mustHearButton.remove();
+  }
+
+  var showMoreStats = document.querySelector(".action.showMoreStats");
+  if (showMoreStats) {
+    showMoreStats.click();
+  }
+
+  var selectedTab = document.querySelector(".selectBox.selected");
+  if (selectedTab && selectedTab.textContent.trim() === "User Reviews") {
+    selectedTab.textContent = "Reviews";
+  }
+
+  var headline = document.querySelector(".albumHeadline");
+  var coverBox = document.querySelector(".albumTopBox.cover");
+  var ratingBox = document.querySelector(".albumTopBox:not(.cover):not(.info)");
+  if (headline && ratingBox) {
+    ratingBox.insertBefore(headline, ratingBox.firstChild);
+  } else if (headline && coverBox && coverBox.parentNode) {
+    coverBox.parentNode.insertBefore(headline, coverBox.nextSibling);
+  }
+
+  var style = document.createElement("style");
+  style.textContent =
+    ".albumUserScoreBox{border-top-left-radius:6px;border-top-right-radius:6px;}" +
+    ".albumTopBox .albumHeadline{text-align:center;}";
+  (document.head || document.documentElement).appendChild(style);
+})();
